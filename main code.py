a93854f869e1b69f49dc1e98604643c0adfeb4f1
satyatechnicalgyan <67205672+satyatechnicalgyan@users.noreply.github.com>
2020-07-15 07:31:07 +0530

    Update main code.py

--- main code.py
+++ main code.py
@@ -2,7 +2,7 @@ from Rpi import GPIO       #import GPIO Module
 GPIO.setmode(GPIO.BOARD)   # Board Pin 
 GPIO.setwarnings(False)    # set Warnings
 GPIO.setup(40,GPIO.IN)     #declare input pin
-GPIO.setup(5,GPIO.OUT)  #declare output pin
+GPIO.setup(5,GPIO.OUT)     #declare output pin
 while True:                #use to While loop
   a=GPIO.input(40)         #input pin 
   if a==1:                 #condition
