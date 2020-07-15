25fc26ee897ee1eb4c17e5448b8b6e4d0360f9ab
satyatechnicalgyan <67205672+satyatechnicalgyan@users.noreply.github.com>
2020-07-15 06:51:22 +0530

    Create main code.py

---
+++ main code.py
@@ -0,0 +1,12 @@
+from Rpi import GPIO       #import GPIO Module
+GPIO.setmode(GPIO.BOARD)   # Board Pin 
+GPIO.setwarnings(False)    # set Warnings
+GPIO.setup(40,GPIO.IN)     #declare input pin
+GPIO.setup(5,GPIO.OUTPUT)  #declare output pin
+while True:                #use to While loop
+  a=GPIO.input(40)         #input pin 
+  if a==1:                 #condition
+    GPIO.output(5,HIGH)    #Led On
+  else:
+    GPIO.output(5,LOW)     #Led Off
+  
